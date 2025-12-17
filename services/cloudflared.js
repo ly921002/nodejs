6248f931e5dad63d10b5c74ff08692046695a12b
ly921002 <92194283+ly921002@users.noreply.github.com>
2025-12-17 16:20:17 +0800

    Create cloudflared.js

---
+++ services/cloudflared.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const os = require('os');
+const { downloadFile } = require('../core/download');
+const { retry } = require('../core/utils');
+
+function arch() {
+  return os.arch().includes('arm') ? 'arm' : 'amd';
+}
+
+async function downloadCloudflared(binPath, version) {
+  if (fs.existsSync(binPath)) return;
+
+  const url = arch() === 'arm'
+    ? `https://github.com/cloudflare/cloudflared/releases/download/${version}/cloudflared-linux-arm64`
+    : `https://github.com/cloudflare/cloudflared/releases/download/${version}/cloudflared-linux-amd64`;
+
+  await retry(() => downloadFile(url, binPath));
+  fs.chmodSync(binPath, 0o755);
+}
+
+module.exports = { downloadCloudflared };
