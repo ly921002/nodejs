e2c3deec513886faf56af81e54d28599991406e2
ly921002 <92194283+ly921002@users.noreply.github.com>
2025-12-17 16:24:12 +0800

    Update http.js

--- server/http.js
+++ server/http.js
@@ -11,7 +11,16 @@ function startServer(port, subPath) {
     res.type('text/plain').send(state.sub);
   });
 
-  app.get('/', (_, res) => res.send('Service running'));
+  app.get('/', async (req, res) => {
+    try {
+      // 尝试读取当前目录下的 index.html
+      const html = await fs.promises.readFile(path.join(__dirname, 'index.html'), 'utf8');
+      res.send(html);
+    } catch (err) {
+      // 如果读取失败（文件不存在等），返回基础状态
+      res.send('Service is running. Visit /' + SUB_PATH);
+    }
+  });
   app.listen(port);
 }
 
